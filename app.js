f37f04c23538b8ec40d0ee349c9dbb45d8a92b56
Arny <arne.n.bjerkaas@gmail.com>
2018-07-04 20:33:49 +0200

    fix first stage of js

--- app.js
+++ app.js
@@ -4,12 +4,35 @@ const scissors = document.getElementById('scissors');
 const win = document.getElementById('win');
 const lose = document.getElementById('lose');
 
+//on click return value of r,p or s
 rock.addEventListener('click', function(){
-  console.log("YES!");
+  return "r";
 });
 paper.addEventListener('click', function(){
-  console.log("YES!");
+  return "p";
 });
 scissors.addEventListener('click', function(){
-  console.log("YES!");
+  return "s";
 });
+
+//random function for computers choice 0-2, runs after click
+function computer(choice){
+  let randomNum = Math.floor(Math.random() * 3);
+  let letterArr = ["r", "p", "s"];
+  return letterArr[randomNum];
+}
+console.log(computer());
+console.log(roc());
+let computerCoice = computer();
+
+//compare computers choice with users choice
+  if(paper == computerCoice){
+    console.log("TIE!!!!");
+  }
+
+
+
+//if win or lose increment scoreBoard
+//reset button
+//background when win
+//background when lose
