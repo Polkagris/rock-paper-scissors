b72a579f3cc369b57f6935e9d498aeb65bf42b8c
Arny <arne.n.bjerkaas@gmail.com>
2018-07-04 20:08:03 +0200

    initial setup

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-ECHO is on.
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const win = document.getElementById('win');
+const lose = document.getElementById('lose');
+
+rock.addEventListener('click', function(){
+  console.log("YES!");
+});
+paper.addEventListener('click', function(){
+  console.log("YES!");
+});
+scissors.addEventListener('click', function(){
+  console.log("YES!");
+});
