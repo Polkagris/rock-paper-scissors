d43d292942235c94a520ab8d8e35dcbd15908a7c
Arny <arne.n.bjerkaas@gmail.com>
2018-07-05 14:48:08 +0200

    fix scoreboard logic

--- app.js
+++ app.js
@@ -4,31 +4,66 @@ const scissors = document.getElementById('scissors');
 const win = document.getElementById('win');
 const lose = document.getElementById('lose');
 
+function game(usersHand){
+  const computersHand = computer();
+  console.log("This is the users hand: " + usersHand);
+  console.log("This is the computers hand: " + computersHand);
+
+function compare(usersHand, computersHand){
+  let answer = "";
+    switch(usersHand + computersHand){
+      case("rr"):
+      case("ss"):
+      case("pp"):
+        answer = "DRAW!";
+        break;
+      case("rs"):
+      case("sp"):
+      case("pr"):
+        answer = "WIN!";
+        break;
+      case("rp"):
+      case("sr"):
+      case("ps"):
+        answer = "LOSE!";
+        break
+      default:
+      answer = "something else";
+      break;
+    }
+    return answer;
+}
+  console.log(compare(usersHand, computersHand));
+  if(compare(usersHand, computersHand) == "WIN!"){
+    console.log("WE HAVE A WINNER!");
+    win.innerHTML = parseInt(win.innerHTML) + 1;
+} else if (compare(usersHand, computersHand) == "LOSE!"){
+  lose.innerHTML = parseInt(lose.innerHTML) + 1;
+}
+
+}
+
+
 //on click return value of r,p or s
 rock.addEventListener('click', function(){
-  return "r";
+   game("r");
 });
 paper.addEventListener('click', function(){
-  return "p";
+  game("p");
 });
 scissors.addEventListener('click', function(){
-  return "s";
+  game("s");
 });
 
 //random function for computers choice 0-2, runs after click
-function computer(choice){
+function computer(){
   let randomNum = Math.floor(Math.random() * 3);
   let letterArr = ["r", "p", "s"];
   return letterArr[randomNum];
 }
-console.log(computer());
-console.log(roc());
-let computerCoice = computer();
 
 //compare computers choice with users choice
-  if(paper == computerCoice){
-    console.log("TIE!!!!");
-  }
+
 
 
 
